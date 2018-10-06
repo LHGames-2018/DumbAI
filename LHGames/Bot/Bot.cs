7e243ae1bb85000543db266195f8528876e6e33b
antoine lamontagne <antoine.lamontagne@hotmail.ca>
2018-10-06 11:43:15 -0400

    j'ai peur

--- LHGames/Bot/Bot.cs
+++ LHGames/Bot/Bot.cs
@@ -18,6 +18,7 @@ namespace LHGames.Bot
         internal void BeforeTurn(IPlayer playerInfo)
         {
             PlayerInfo = playerInfo;
+            Console.WriteLine(playerInfo);
         }
 
         /// <summary>
@@ -28,15 +29,48 @@ namespace LHGames.Bot
         /// <returns>The action you wish to execute.</returns>
         internal string ExecuteTurn(Map map, IEnumerable<IPlayer> visiblePlayers)
         {
-            // TODO: Implement your AI here.
-            if (map.GetTileAt(PlayerInfo.Position.X + _currentDirection, PlayerInfo.Position.Y) == TileContent.Wall)
+            Point pointToGo;
+            if (PlayerInfo.CarriedResources == PlayerInfo.CarryingCapacity)
             {
-                _currentDirection *= -1;
+                pointToGo = PlayerInfo.HouseLocation;
+            }
+            else
+            {
+                pointToGo = StorageHelper.Read<Point>("pointToGo");
+            }
+            if (pointToGo != null || map.GetTileAt(pointToGo.X, pointToGo.Y) != TileContent.Resource)
+            {
+                foreach (Tile tile in map.GetVisibleTiles())
+                {
+                    if (tile.TileType == TileContent.Resource)
+                    {
+                        pointToGo = tile.Position;
+                        break;
+                    }
+                }
+            }
+            StorageHelper.Write("pointToGo", pointToGo);
+            Point deplacement = pointToGo - PlayerInfo.Position;
+            Point mouvement;
+            if (Math.Abs(deplacement.X) <= 1 && (PlayerInfo.HouseLocation != pointToGo || deplacement.X == 0))
+            {
+                if(Math.Abs(deplacement.Y) == 1 && pointToGo != PlayerInfo.HouseLocation)
+                {
+                    mouvement = new Point(0, deplacement.Y / Math.Abs(deplacement.Y));
+                }
+                else
+                {
+                    return AIHelper.CreateCollectAction(deplacement);
+                }
+            }
+            else
+            {
+                mouvement = new Point(deplacement.X / Math.Abs(deplacement.X), 0);
             }
 
             var data = StorageHelper.Read<TestClass>("Test");
             Console.WriteLine(data?.Test);
-            return AIHelper.CreateMoveAction(new Point(_currentDirection, 0));
+            return AIHelper.CreateMoveAction(mouvement);
         }
 
         /// <summary>
