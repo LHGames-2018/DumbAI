d7f0a9d965ab99d8e0520deda90919d0f73b4d84
Marilou Farmer <marilou@clinia.ca>
2018-10-06 16:06:26 -0400

    New A*

--- LHGames/Bot/Bot.cs
+++ LHGames/Bot/Bot.cs
@@ -5,278 +5,266 @@ using LHGames.Helper;
 
 namespace LHGames.Bot
 {
-    internal class Bot
-    {
-        internal IPlayer PlayerInfo { get; set; }
-        private int _currentDirection = 1;
-        List<InterestingObject> objects = new List<InterestingObject>();
-        //InterestingObject currentObject = null;
+   internal class Bot
+   {
+      internal IPlayer PlayerInfo { get; set; }
+      private int _currentDirection = 1;
+      List<InterestingObject> objects = new List<InterestingObject>();
+      //InterestingObject currentObject = null;
 
-        internal Bot() { }
+      internal Bot() { }
 
-        /// <summary>
-        /// Gets called before ExecuteTurn. This is where you get your bot's state.
-        /// </summary>
-        /// <param name="playerInfo">Your bot's current state.</param>
-        internal void BeforeTurn(IPlayer playerInfo)
-        {
-            PlayerInfo = playerInfo;
-        }
+      /// <summary>
+      /// Gets called before ExecuteTurn. This is where you get your bot's state.
+      /// </summary>
+      /// <param name="playerInfo">Your bot's current state.</param>
+      internal void BeforeTurn(IPlayer playerInfo)
+      {
+         PlayerInfo = playerInfo;
+      }
 
-        /// <summary>
-        /// Implement your bot here.
-        /// </summary>
-        /// <param name="map">The gamemap.</param>
-        /// <param name="visiblePlayers">Players that are visible to your bot.</param>
-        /// <returns>The action you wish to execute.</returns>
-        internal string ExecuteTurn(Map map, IEnumerable<IPlayer> visiblePlayers)
-        {
-            getInterestingObjects(map, PlayerInfo.Position);
-            objects.Sort((x, y) => x.priority.CompareTo(y.priority));
-            PathFinder pathfinder;
-            if (PlayerInfo.CarriedResources == PlayerInfo.CarryingCapacity)
+      /// <summary>
+      /// Implement your bot here.
+      /// </summary>
+      /// <param name="map">The gamemap.</param>
+      /// <param name="visiblePlayers">Players that are visible to your bot.</param>
+      /// <returns>The action you wish to execute.</returns>
+      internal string ExecuteTurn(Map map, IEnumerable<IPlayer> visiblePlayers)
+      {
+         getInterestingObjects(map, PlayerInfo.Position);
+         objects.Sort((x, y) => x.priority.CompareTo(y.priority));
+         PathFinder pathfinder;
+         if (PlayerInfo.CarriedResources == PlayerInfo.CarryingCapacity)
+         {
+            pathfinder = new PathFinder(map, PlayerInfo.Position, PlayerInfo.HouseLocation);
+         }
+         else
+         {
+            pathfinder = new PathFinder(map, PlayerInfo.Position, objects[0].position);
+         }
+         Point nextMove = pathfinder.FindNextMove();
+         Point shouldIStayOrShouldIGoNow = PlayerInfo.Position + nextMove;
+
+         TileContent content = map.GetTileAt(shouldIStayOrShouldIGoNow.X, shouldIStayOrShouldIGoNow.Y);
+         switch (content)
+         {
+            case TileContent.Empty:
+            case TileContent.House:
+               return AIHelper.CreateMoveAction(nextMove);
+            case TileContent.Resource:
+               return AIHelper.CreateCollectAction(nextMove);
+            case TileContent.Wall:
+               return AIHelper.CreateMeleeAttackAction(nextMove);
+            case TileContent.Player:
+               return AIHelper.CreateMeleeAttackAction(nextMove);
+            default:
+               return AIHelper.CreateEmptyAction();
+         }
+      }
+
+      /// <summary>
+      /// Gets called after ExecuteTurn.
+      /// </summary>
+      internal void AfterTurn()
+      {
+      }
+
+      internal void getInterestingObjects(Map map, Point ownPosition)
+      {
+         List<InterestingObject> tmpList = new List<InterestingObject>();
+         List<Tile> visibleTiles = new List<Tile>(map.GetVisibleTiles());
+         for (int i = 0; i < visibleTiles.Count; i++)
+         {
+            Tile currentTile = visibleTiles[i];
+            InterestingObject currentObject = null;
+            switch ((int)currentTile.TileType)
             {
-                pathfinder = new PathFinder(map, PlayerInfo.Position, PlayerInfo.HouseLocation);
+               case 4:
+                  currentObject = new InterestingObject() { distanceFromUser = new int[2] { currentTile.Position.X - PlayerInfo.Position.X, currentTile.Position.Y - PlayerInfo.Position.Y }, type = currentTile.TileType, position = currentTile.Position };
+                  currentObject.priority = Math.Abs(currentObject.distanceFromUser[0]) + Math.Abs(currentObject.distanceFromUser[1]);
+                  break;
+
+               case 6:
+                  currentObject = new InterestingObject() { distanceFromUser = new int[2] { currentTile.Position.X - PlayerInfo.Position.X, currentTile.Position.Y - PlayerInfo.Position.Y }, type = currentTile.TileType, position = currentTile.Position };
+                  currentObject.priority = Math.Abs(currentObject.distanceFromUser[0]) + Math.Abs(currentObject.distanceFromUser[1]) - 5; //+5 to make the enemies always prioritary over resources
+                  break;
+
+               default:
+                  break;
+
             }
-            else
-            {
-                pathfinder = new PathFinder(map, PlayerInfo.Position, objects[0].position);
-            }
-            Point nextMove = pathfinder.FindNextMove();
-            Point shouldIStayOrShouldIGoNow = PlayerInfo.Position + nextMove;
-
-            TileContent content = map.GetTileAt(shouldIStayOrShouldIGoNow.X, shouldIStayOrShouldIGoNow.Y);
-            switch (content)
-            {
-                case TileContent.Empty:
-                case TileContent.House:
-                    return AIHelper.CreateMoveAction(nextMove);
-                case TileContent.Resource:
-                    return AIHelper.CreateCollectAction(nextMove);
-                case TileContent.Wall:
-                    return AIHelper.CreateMeleeAttackAction(nextMove);
-                case TileContent.Player:
-                    return AIHelper.CreateMeleeAttackAction(nextMove);
-                default:
-                    return AIHelper.CreateEmptyAction();
-            }
-        }
-
-        /// <summary>
-        /// Gets called after ExecuteTurn.
-        /// </summary>
-        internal void AfterTurn()
-        {
-        }
-
-        internal void getInterestingObjects(Map map, Point ownPosition)
-        {
-            List<InterestingObject> tmpList = new List<InterestingObject>();
-            List<Tile> visibleTiles = new List<Tile>(map.GetVisibleTiles());
-            for (int i = 0; i < visibleTiles.Count; i++)
-            {
-                Tile currentTile = visibleTiles[i];
-                InterestingObject currentObject = null;
-                switch ((int)currentTile.TileType)
-                {
-                    case 4:
-                        currentObject = new InterestingObject() { distanceFromUser = new int[2] { currentTile.Position.X - PlayerInfo.Position.X, currentTile.Position.Y - PlayerInfo.Position.Y }, type = currentTile.TileType, position = currentTile.Position };
-                        currentObject.priority = Math.Abs(currentObject.distanceFromUser[0]) + Math.Abs(currentObject.distanceFromUser[1]);
-                        break;
-
-                    case 6:
-                        currentObject = new InterestingObject() { distanceFromUser = new int[2] { currentTile.Position.X - PlayerInfo.Position.X, currentTile.Position.Y - PlayerInfo.Position.Y }, type = currentTile.TileType, position = currentTile.Position };
-                        currentObject.priority = Math.Abs(currentObject.distanceFromUser[0]) + Math.Abs(currentObject.distanceFromUser[1]) - 5; //+5 to make the enemies always prioritary over resources
-                        break;
-
-                    default:
-                        break;
-
-                }
-                if (currentObject != null && currentObject.position != ownPosition)
-                    tmpList.Add(currentObject);
-                objects = tmpList;
-            }
-        }
-    }
+            if (currentObject != null && currentObject.position != ownPosition)
+               tmpList.Add(currentObject);
+            objects = tmpList;
+         }
+      }
+   }
 }
 
 
 
 class TestClass
 {
-    public string Test { get; set; }
+   public string Test { get; set; }
 }
 
 internal class PathFinder
 {
-    readonly Node[,] nodes;
-    private Point overflow;
-    readonly Node startNode;
-    readonly Node endNode;
+   readonly Node[,] nodes;
+   private Point overflow;
+   readonly Node startNode;
+   readonly Node endNode;
 
-    public Point FindNextMove()
-    {
-        if (overflow != null)
-        {
-            return overflow;
-        }
-        List<Point> path = FindPath();
-        return path.Count > 0 ? path[0] - startNode.Location : new Point(0, 0);
-    }
+   public Point FindNextMove()
+   {
+      if (overflow != null)
+      {
+         return overflow;
+      }
+      List<Point> path = FindPath();
+      return path.Count > 0 ? path[0] - startNode.Location : new Point(0, 0);
+   }
 
-    public PathFinder(Map map, Point start, Point end)
-    {
-        int mapSize = map.VisibleDistance * 2 + 1;
-        int deltaX = start.X - map.VisibleDistance;
-        int deltaY = start.Y - map.VisibleDistance;
-        nodes = new Node[mapSize, mapSize];
-        for (int i = 0; i < mapSize; i++)
-        {
-            for (int j = 0; j < mapSize; j++)
+   public PathFinder(Map map, Point start, Point end)
+   {
+      int mapSize = map.VisibleDistance * 2 + 1;
+      int deltaX = start.X - map.VisibleDistance;
+      int deltaY = start.Y - map.VisibleDistance;
+      nodes = new Node[mapSize, mapSize];
+      for (int i = 0; i < mapSize; i++)
+      {
+         for (int j = 0; j < mapSize; j++)
+         {
+            TileContent tile = map.GetTileAt(i + deltaX, j + deltaY);
+            nodes[i, j] = new Node(tile == TileContent.Empty || tile == TileContent.Wall || tile == TileContent.House,
+               new Point(i, j),
+               Math.Abs(i + deltaX - end.X) + Math.Abs(j + deltaY - end.Y), tile == TileContent.Wall ? 3 : 1);
+         }
+      }
+
+      startNode = nodes[start.X - deltaX, start.Y - deltaY];
+      int x, y;
+      x = end.X - deltaX;
+      y = end.Y - deltaY;
+      overflow = x < 0 ? new Point(1, 0) : (x >= mapSize ? new Point(-1, 0) : (y < 0 ? new Point(0, 1) : (y >= mapSize ? new Point(0, -1) : null)));
+      if (overflow == null)
+      {
+         endNode = nodes[end.X - deltaX, end.Y - deltaY];
+      }
+   }
+
+   List<Point> FindPath()
+   {
+      List<Point> path = new List<Point>();
+      bool success = Search(startNode);
+      if (success)
+      {
+         Node node = this.endNode;
+         while (node.ParentNode != null)
+         {
+            path.Add(node.Location);
+            node = node.ParentNode;
+         }
+         path.Reverse();
+      }
+      return path;
+   }
+
+   bool Search(Node currentNode)
+   {
+      List<Node> openNodes = new List<Node>();
+      List<Node> closedNodes = new List<Node>();
+      openNodes.Add(currentNode);
+
+      while (openNodes.Count > 0)
+      {
+         openNodes.Sort((x, y) => x.F.CompareTo(y.F));
+         Node parent = openNodes.First();
+
+         List<Node> successors = GetAdjacentNodes(parent).ToList();
+
+         foreach (Node suc in successors)
+         {
+            if (suc == endNode)
             {
-                TileContent tile = map.GetTileAt(i + deltaX, j + deltaY);
-                nodes[i, j] = new Node(tile == TileContent.Empty || tile == TileContent.Wall || tile == TileContent.House,
-                   new Point(i, j),
-                   Math.Abs(i + deltaX - end.X) + Math.Abs(j + deltaY - end.Y), tile == TileContent.Wall ? 4 : 1);
+               suc.ParentNode = parent;
+               return true;
             }
-        }
 
-        startNode = nodes[start.X - deltaX, start.Y - deltaY];
-        int x, y;
-        x = end.X - deltaX;
-        y = end.Y - deltaY;
-        overflow = x < 0 ? new Point(-1, 0) : (x >= mapSize ? new Point(1, 0) : (y < 0 ? new Point(0, -1) : (y >= mapSize ? new Point(0, 1) : null)));
-        if (overflow == null)
-        {
-            endNode = nodes[end.X - deltaX, end.Y - deltaY];
-        }
-    }
-
-    List<Point> FindPath()
-    {
-        List<Point> path = new List<Point>();
-        bool success = Search(startNode);
-        if (success)
-        {
-            Node node = this.endNode;
-            while (node.ParentNode != null)
+            if (suc.IsWalkable)
             {
-                path.Add(node.Location);
-                node = node.ParentNode;
+               if (openNodes.Contains(suc) && suc.G > parent.G + suc.TileValue)
+               {
+                  suc.ParentNode = parent;
+               }
+               else if (closedNodes.Contains(suc))
+               {
+                  if (suc.G > parent.G + suc.TileValue)
+                  {
+                     suc.ParentNode = parent;
+                     openNodes.Add(suc);
+                     closedNodes.Remove(suc);
+                  }
+               }
+               else
+               {
+                  suc.ParentNode = parent;
+                  openNodes.Add(suc);
+               }
             }
-            path.Reverse();
-        }
-        return path;
-    }
 
-    bool Search(Node currentNode)
-    {
-        currentNode.State = NodeState.Closed;
-        List<Node> nextNodes = GetAdjacentWalkableNodes(currentNode);
-        nextNodes.Sort((node1, node2) => node1.F.CompareTo(node2.F));
-        foreach (var nextNode in nextNodes)
-        {
-            if (nextNode.Location == this.endNode.Location)
-            {
-                return true;
-            }
-            else
-            {
-                if (Search(nextNode))
-                    return true;
-            }
-        }
-        return false;
-    }
+         }
+         openNodes.Remove(parent);
+         closedNodes.Add(parent);
+      }
 
-    List<Node> GetAdjacentWalkableNodes(Node fromNode)
-    {
-        List<Node> walkableNodes = new List<Node>();
-        IEnumerable<Point> nextLocations = GetAdjacentLocations(fromNode.Location);
+      return false;
+   }
 
-        foreach (var location in nextLocations)
-        {
-            int x = location.X;
-            int y = location.Y;
+   IEnumerable<Node> GetAdjacentNodes(Node fromNode)
+   {
+      IEnumerable<Point> nextLocations = GetAdjacentLocations(fromNode.Location);
+      return nextLocations.Where(x => x.X >= 0 && x.X < nodes.GetLength(0) && x.Y >= 0 && x.Y < nodes.GetLength(1)).Select(x => this.nodes[x.X, x.Y]);
+   }
 
-            // Stay within the grid's boundaries
-            if (x < 0 || x >= nodes.GetLength(0) || y < 0 || y >= nodes.GetLength(1))
-                continue;
+   List<Point> GetAdjacentLocations(Point pt)
+   {
+      List<Point> adj = new List<Point>();
+      adj.Add(new Point(pt.X - 1, pt.Y));
+      adj.Add(new Point(pt.X + 1, pt.Y));
+      adj.Add(new Point(pt.X, pt.Y + 1));
+      adj.Add(new Point(pt.X, pt.Y - 1));
+      return adj;
+   }
 
-            Node node = this.nodes[x, y];
-            // Ignore non-walkable nodes
-            if (!node.IsWalkable && node != endNode)
-                continue;
+   public class Node
+   {
+      public Point Location { get; private set; }
+      public bool IsWalkable { get; set; }
+      public int TileValue { get; set; }
 
-            // Ignore already-closed nodes
-            if (node.State == NodeState.Closed)
-                continue;
+      public float G
+      {
+         get { return ParentNode == null ? 0 : ParentNode.G + TileValue; }
+      }
+      public float H { get; private set; }
+      public float F { get { return this.G + this.H; } }
+      public Node ParentNode { get; set; }
 
-            // Already-open nodes are only added to the list if their G-value is lower going via this route.
-            if (node.State == NodeState.Open)
-            {
-                float gTemp = fromNode.G + node.TileValue;
-                if (gTemp < node.G)
-                {
-                    node.ParentNode = fromNode;
-                    walkableNodes.Add(node);
-                }
-            }
-            else
-            {
-                // If it's untested, set the parent and flag it as 'Open' for consideration
-                node.ParentNode = fromNode;
-                node.State = NodeState.Open;
-                walkableNodes.Add(node);
-            }
-        }
-
-        return walkableNodes;
-    }
-
-    List<Point> GetAdjacentLocations(Point pt)
-    {
-        List<Point> adj = new List<Point>();
-        adj.Add(new Point(pt.X - 1, pt.Y));
-        adj.Add(new Point(pt.X + 1, pt.Y));
-        adj.Add(new Point(pt.X, pt.Y + 1));
-        adj.Add(new Point(pt.X, pt.Y - 1));
-        return adj;
-    }
-
-    public class Node
-    {
-        public Point Location { get; private set; }
-        public bool IsWalkable { get; set; }
-        public int TileValue { get; set; }
-
-        public float G
-        {
-            get { return ParentNode == null ? 0 : ParentNode.G + TileValue; }
-        }
-        public float H { get; private set; }
-        public float F { get { return this.G + this.H; } }
-        public NodeState State { get; set; }
-        public Node ParentNode { get; set; }
-
-        public Node(bool isWalkable, Point location, float h, int tileValue)
-        {
-            IsWalkable = isWalkable;
-            Location = location;
-            H = h;
-            State = NodeState.Untested;
-            TileValue = tileValue;
-        }
-    }
-
-    public enum NodeState { Untested, Open, Closed }
+      public Node(bool isWalkable, Point location, float h, int tileValue)
+      {
+         IsWalkable = isWalkable;
+         Location = location;
+         H = h;
+         TileValue = tileValue;
+      }
+   }
 }
 
 class InterestingObject
 {
-    public int[] distanceFromUser;
-    public TileContent type;
-    public int priority;
-    public Point position;
+   public int[] distanceFromUser;
+   public TileContent type;
+   public int priority;
+   public Point position;
 }
